1044619bead98da9d71e1eacb62461c99129958a
umeshuday <umeshuday788@gmail.com>
2022-12-31 14:12:52 +0530

    first commit

---
+++ src/redux/counter/counterSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload
+    },
+    emptyCounter : (state) =>{
+        state.value = 0
+    }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount, emptyCounter } = counterSlice.actions
+
+export default counterSlice.reducer
